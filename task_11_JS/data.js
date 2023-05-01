c393c3736a33b1b01958cfcc9bfe5b7621ab0912
BarhatovaIrina <biriska@mail.ru>
2023-05-01 22:04:30 +0300

    Задание 11

---
+++ task_11_JS/data.js
@@ -0,0 +1,34 @@
+let products = [
+    {
+        name: 'Куртка',
+        color: 'Белый',
+        size: 'М',
+        count: 1,
+        price: 7999,
+        img: 'jacket.jpg'
+    },
+    {
+        name: 'Кроссовки',
+        color: 'Черный',
+        size: '39',
+        count: 1,
+        price: 2999,
+        img: 'kros.jpg'
+    },
+    {
+        name: 'Джинсы',
+        color: 'Голубой',
+        size: 'М',
+        count: 1,
+        price: 1999,
+        img: 'jeans.jpg'
+    },
+    {
+        name: 'Платье',
+        color: 'Черный',
+        size: 'S',
+        count: 1,
+        price: 3999,
+        img: 'dress.jpg'
+    },
+]
