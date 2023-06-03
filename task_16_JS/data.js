275b34497800e289b7057700baee52dbefacefa0
BarhatovaIrina <biriska@mail.ru>
2023-06-03 22:31:58 +0300

    task_16_JS

---
+++ task_16_JS/data.js
@@ -0,0 +1,94 @@
+auto = [
+    {
+        'marka': 'Renault',
+        'models': [
+            {
+                'name': 'Arkana',
+                'price': 2200000
+            },
+            {
+                'name': 'Duster',
+                'price': 1950000
+            },
+
+            {
+                'name': 'Kaptur',
+                'price': 1960000
+            },
+            {
+                'name': 'Logan',
+                'price': 1280000
+            },
+            {
+                'name': 'Sandero',
+                'price': 1530000
+            },
+        ],
+    },
+    {
+        'marka': 'Opel',
+        'models': [
+            {
+                'name': 'Crossland',
+                'price': 2600000
+            },
+            {
+                'name': 'Grandland ',
+                'price': 2750000
+            },
+
+            {
+                'name': 'Vivaro',
+                'price': 3160000
+            },
+            {
+                'name': 'Zafira',
+                'price': 4280000
+            }
+        ],
+    },
+    {
+        'marka': 'Mazda',
+        'models': [
+            {
+                'name': 'CX-9',
+                'price': 4600000
+            },
+            {
+                'name': 'CX-6',
+                'price': 3750000
+            },
+
+            {
+                'name': 'CX-5',
+                'price': 2560000
+            },
+            {
+                'name': 'Mazda6 ',
+                'price': 2280000
+            }
+        ],
+    },
+    {
+        'marka': 'Jaguar',
+        'models': [
+            {
+                'name': 'Jaguar E-PACE',
+                'price': 4100000
+            },
+            {
+                'name': ' Jaguar XF',
+                'price': 4600000
+            },
+
+            {
+                'name': 'Jaguar F-PACE',
+                'price': 4560000
+            },
+            {
+                'name': 'Jaguar F-type',
+                'price': 5280000
+            }
+        ],
+    },
+]
